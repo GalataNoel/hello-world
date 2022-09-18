386e3f7243198e6924272d9a056cc2365f734786
Galata Noel <33789400+GalataNoel@users.noreply.github.com>
2022-09-18 10:52:44 +0200

    Add files via upload

---
+++ main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Neved: ");
+        String nev = in.nextLine();
+        System.out.println("Nyelved: ");
+        String nyelv = in.nextLine();
+        if(nyelv.equalsIgnoreCase("magyar")){
+            System.out.println("Szia "+nev);
+        }else if(nyelv.equalsIgnoreCase("német")){
+            System.out.println("Tschüs "+nev);
+        }else if(nyelv.equalsIgnoreCase("angol")){
+            System.out.println("Hello "+nev);
+        }else
+            System.out.println("Hiba!");
+    }
+}
